fb13b5b1fc885cd130638a45ddda31fee78e256d
Manan Shah <mananshah3654@gmail.com>
2023-06-13 19:38:25 +0530

    MVC in Action

---
+++ Cloudly/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using Cloudly.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cloudly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movies = new Movies() { Name = "Adipurush !" };
+            return View(movies);
+        }
+    }
+}
