d5eaa8b534491877ea5f2eb2df773ff58395cf4c
Manan Shah <mananshah3654@gmail.com>
2023-06-16 20:43:20 +0530

    Use a View Model

--- Cloudly/Controllers/MoviesController.cs
+++ Cloudly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Cloudly.ViewModel;
 
 namespace Cloudly.Controllers
 {
@@ -13,48 +14,21 @@ namespace Cloudly.Controllers
         public ActionResult Random()
         {
             var movies = new Movies() { Name = "Adipurush !" };
-            
-            //* Different types of Action Results
-            
-            //return new ViewResult();
-            //return Content("Hello Word!");
-            //return HttpNotFound();
-            //return RedirectToAction("Index", "Home");
-            return View(movies);
-        }
 
-
-        #region Action Parameters
-
-        public ActionResult edit(int movieId)
-        {
-            return Content("id = " + movieId);
-        }
-
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if(!pageIndex.HasValue)
+            var customers = new List<Customers>
             {
-                pageIndex = 1;
-            }
-            
-            if (String.IsNullOrWhiteSpace(sortBy))
+                new Customers { Id=1, Name="Customer 1"},
+                new Customers { Id=2, Name="Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
             {
-                sortBy = "Name";
-            }
+                Movie = movies,
+                Customer = customers
+            };
 
-            return Content(String.Format("pageIndex={0}&sortBy={1}" , pageIndex, sortBy));
-        }
-        #endregion
-
-        #region Conventional & Attribute Routing
-
-        [Route("movies/released/{year}/{month}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
+            return View(viewModel);
         }
 
-        #endregion
     }
 }
---
+++ Cloudly/ViewModel/RandomMovieViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Cloudly.Models;
+
+namespace Cloudly.ViewModel
+{
+    public class RandomMovieViewModel
+    {
+        public List<Customers> Customer;
+
+        public Movies Movie;
+    }
+}
